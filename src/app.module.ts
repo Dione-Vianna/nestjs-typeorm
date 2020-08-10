f5bc4a7993ae5f2bacd14a65beec3ea39f33c9d6
dione-vianna <dioneg12@hotmail.com>
2020-08-10 15:47:46 -0300

    criado os asquivos de service e module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import RepoModule from './repo.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import AuthorResolver from './resolvers/author.resolver';
+import BookResolver from './resolvers/book.resolver';
+import GenreResolver from './resolvers/genre.resolver';
+import BookGenreResolver from './resolvers/book-genre.resolver';
+import { genreBooksLoader } from './db/loaders/books.loader';
+
+const graphQLImports = [
+  AuthorResolver,
+  BookResolver,
+  GenreResolver,
+  BookGenreResolver,
+];
 
 @Module({
-  imports: [],
+
+  imports: [TypeOrmModule.forRoot(),
+    RepoModule,
+    ...graphQLImports,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+      context: {
+        genreBooksLoader: genreBooksLoader(),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
